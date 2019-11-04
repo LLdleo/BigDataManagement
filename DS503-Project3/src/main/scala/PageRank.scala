554de43f075a488f47cc2491e33fb6c4be9a6aac
Leo <lldlld0574@gmail.com>
2019-11-04 00:06:21 -0500

    Project3 finished

---
+++ DS503-Project3/src/main/scala/PageRank.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object PageRank {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("PageRank Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder().appName("PageRank Application SQL").master("local").getOrCreate()
+    import spark.implicits._
+    var links = sc.textFile("input/soc-LiveJournal1.txt").map(line => (line.split("\t")(0), line.split("\t")(1)))
+    val header = links.first()
+    links = links.filter(row=>row!=header)
+    val linkGroup = links.groupByKey().map(pair=>(pair._1,pair._2.toArray))
+    val linkGroupCount = linkGroup.map(pair=>(pair._1,pair._2.length))
+//    linkGroupCount.take(50).foreach(println)
+//    linkGroup.collect().take(50).foreach(println)
+
+    var ranks = linkGroup.map(pair=>(pair._1,1.0))
+
+    for (_ <- 1 to 50) {
+      val contribs = linkGroup.join(ranks,2)
+      val flatMapRDD = contribs.flatMap{
+        case (url,(linkGroup,rank)) => linkGroup.map(dest=>(dest,rank/linkGroup.length))
+      }
+      val reduceByKeyRDD = flatMapRDD.reduceByKey(_ + _,2)
+      ranks=reduceByKeyRDD.map(pair=>(pair._1,(0.15 + 0.85 * pair._2).toFloat))
+    }
+
+//    ranks.collect().take(50).foreach(println)
+    sc.parallelize(ranks.sortBy(_._2,ascending=false).take(100)).saveAsTextFile("output/PageRank")
+
+
+  }
+}
